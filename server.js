603e82099a100b4d8b9687707dc1a029a2b6b6f3
Arisson Lima <arissonlima@gmail.com>
2024-11-21 17:56:56 -0300

    terceira aula finalizada

--- server.js
+++ server.js
@@ -1,42 +1,11 @@
 import express from "express";
-
-const posts = [
-  {
-    id: 1,
-    descricao: "Uma descrição qualquer",
-    imagem: "https://placecats.com/millie/300/100"
-  },
-  {
-    id: 2,
-    descricao: "Uma descrição qualquer 1",
-    imagem: "https://placecats.com/millie/300/100"
-  },
-  {
-    id: 3,
-    descricao: "Uma descrição qualquer 2",
-    imagem: "https://placecats.com/millie/300/100"
-  },
-  {
-    id: 4,
-    descricao: "Uma descrição qualquer 3",
-    imagem: "https://placecats.com/millie/300/100"
-  },
-  {
-    id: 5,
-    descricao: "Uma descrição qualquer 4",
-    imagem: "https://placecats.com/millie/300/100"
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
   console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
 });
 
 function buscarPostPorId(id) {
